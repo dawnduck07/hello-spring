bdc43f6926656a39bef35ac5ccdb75b326af77ae
plasma07 <parkjongseo07@kakao.com>
2023-01-27 09:05:40 +0900

    TDD 테스트

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,52 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * ctrl + shift + t : 해당 클래스의 서비스 클래스를 만들어 준다.
+ */
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 외부에서 memberRepository를 넣어준다.
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     *
+     * ifPresent()
+     * - void
+     * - Optional 객체가 값을 가지고 있는지 확인 후 예외처리
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
